7ee23191a171651a0d9db649d5609d0989d92c7b
OlivierRaginel <OlivierRaginel@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2012-05-04 13:27:49 +0000

    Item11808: perltidy them all, so people are not accidentally impacted by the new enforced rule -- Sorry Micha, could not find a way to define specific values within the file
    git-svn-id: http://svn.foswiki.org/trunk/SvgPlugin@14686 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- lib/TWiki/Plugins/SvgPlugin.pm
+++ lib/TWiki/Plugins/SvgPlugin.pm
@@ -10,7 +10,7 @@
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details, published at 
+# GNU General Public License for more details, published at
 # http://www.gnu.org/copyleft/gpl.html
 #
 # =========================
@@ -40,24 +40,23 @@
 #   getSessionValueHandler  ( $key )                                1.010  Use only in one Plugin
 #   setSessionValueHandler  ( $key, $value )                        1.010  Use only in one Plugin
 #
-# initPlugin is required, all other are optional. 
+# initPlugin is required, all other are optional.
 # For increased performance, all handlers except initPlugin are
 # disabled. To enable a handler remove the leading DISABLE_ from
 # the function name. Remove disabled handlers you do not need.
 #
-# NOTE: To interact with TWiki use the official TWiki functions 
+# NOTE: To interact with TWiki use the official TWiki functions
 # in the TWiki::Func module. Do not reference any functions or
 # variables elsewhere in TWiki!!
-
 
 # =========================
 package TWiki::Plugins::SvgPlugin;
 
 # =========================
 use vars qw(
-        $web $topic $user $installWeb $VERSION $RELEASE $pluginName
-        $debug $defaultSize
-    );
+  $web $topic $user $installWeb $VERSION $RELEASE $pluginName
+  $debug $defaultSize
+);
 
 use Image::LibRSVG;
 
@@ -71,137 +70,131 @@ $VERSION = '$Rev$';
 # of the version number in PLUGINDESCRIPTIONS.
 $RELEASE = 'Dakar';
 
-$pluginName = 'SvgPlugin';  # Name of this Plugin
+$pluginName = 'SvgPlugin';    # Name of this Plugin
 
 # =========================
-sub initPlugin
-{
-  ( $topic, $web, $user, $installWeb ) = @_;
+sub initPlugin {
+    ( $topic, $web, $user, $installWeb ) = @_;
 
-  # check for Plugins.pm versions
-  if( $TWiki::Plugins::VERSION < 1.000 ) {
-      TWiki::Func::writeWarning( "Version mismatch between $pluginName and Plugins.pm" );
-      return 0;
-  }
+    # check for Plugins.pm versions
+    if ( $TWiki::Plugins::VERSION < 1.000 ) {
+        TWiki::Func::writeWarning(
+            "Version mismatch between $pluginName and Plugins.pm");
+        return 0;
+    }
 
-  # Get plugin debug flag
-  $debug = TWiki::Func::getPluginPreferencesFlag("DEBUG");
+    # Get plugin debug flag
+    $debug = TWiki::Func::getPluginPreferencesFlag("DEBUG");
 
-  # get a default size for pictures
-  $defaultSize = TWiki::Func::getPluginPreferencesValue("DEFAULTSIZE");
-  if (not($defaultSize =~ m/([0-9]+)x([0-9]+)/))
-  {
-    $defaultSize = "320x200";
-  }
+    # get a default size for pictures
+    $defaultSize = TWiki::Func::getPluginPreferencesValue("DEFAULTSIZE");
+    if ( not( $defaultSize =~ m/([0-9]+)x([0-9]+)/ ) ) {
+        $defaultSize = "320x200";
+    }
 
-  # Plugin correctly initialized
-  TWiki::Func::writeDebug( "- TWiki::Plugins::${pluginName}::initPlugin( $web.$topic ) is OK" ) if $debug;
-  return 1;
+    # Plugin correctly initialized
+    TWiki::Func::writeDebug(
+        "- TWiki::Plugins::${pluginName}::initPlugin( $web.$topic ) is OK")
+      if $debug;
+    return 1;
 }
 
 # =========================
-sub commonTagsHandler
-{
+sub commonTagsHandler {
 ### my ( $text, $topic, $web ) = @_;   # do not uncomment, use $_[0], $_[1]... instead
 
-  TWiki::Func::writeDebug( "- ${pluginName}::commonTagsHandler( $_[2].$_[1] )" ) if $debug;
+    TWiki::Func::writeDebug("- ${pluginName}::commonTagsHandler( $_[2].$_[1] )")
+      if $debug;
 
-  # do custom extension rule, like for example:
-  # $_[0] =~ s/%XYZ%/&handleXyz()/ge;
-  # $_[0] =~ s/%XYZ{(.*?)}%/&handleXyz($1)/ge;
-    
-  $_[0] =~ s/%SVG{(.*?)}%/&handleSvg($1,$_[1],$_[2])/ge;
-    
+    # do custom extension rule, like for example:
+    # $_[0] =~ s/%XYZ%/&handleXyz()/ge;
+    # $_[0] =~ s/%XYZ{(.*?)}%/&handleXyz($1)/ge;
+
+    $_[0] =~ s/%SVG{(.*?)}%/&handleSvg($1,$_[1],$_[2])/ge;
+
 }
 
-sub SvgPluginError
-{
-  my ($arg) = @_;
-  return "<span style='background: #FFFFCC; color: red; text-decoration: underline;'><strong>$pluginName:</strong> $arg</span>";
+sub SvgPluginError {
+    my ($arg) = @_;
+    return
+"<span style='background: #FFFFCC; color: red; text-decoration: underline;'><strong>$pluginName:</strong> $arg</span>";
 }
 
-sub handleSvg
-{
-  my ($args,$topic,$web) = @_;
+sub handleSvg {
+    my ( $args, $topic, $web ) = @_;
 
-  # which drawing would we convert?
-  my $drawing;
-  if ($args =~ m/^"([^"]+)"/)
-  {
-    $drawing = $1;
-  }
-  else
-  {
-    return SvgPluginError("you must specify a drawing to display!");
-  }
-
-  #where is the drawing?
-  my $where;
-  if ($args =~ m/topic="(([^\.]+)\.)?([^"]+)"/)
-  {
-    if ($2)
-    {
-      # given a complete topic name, i.e. Web.TheTopic
-      $where = "$2/$3";
+    # which drawing would we convert?
+    my $drawing;
+    if ( $args =~ m/^"([^"]+)"/ ) {
+        $drawing = $1;
     }
-    else
-    {
-      # given only a topic name, use current web.
-      $where = "$web/$3";
+    else {
+        return SvgPluginError("you must specify a drawing to display!");
     }
-  }
-  else
-  {
-    # nothing given, use current topic
-    $where = "$web/$topic";
-  }
 
-  # calculate size of the generated image:
-  my ($width,$height);
-  if ($args =~ m/size="([0-9]+)x([0-9]+)"/)
-  {
-    $width = $1;
-    $height = $2;
-  }
-  else
-  {
-    $defaultSize =~ m/([0-9]+)x([0-9]+)/;
-    $width = $1;
-    $height = $2;
-  }
+    #where is the drawing?
+    my $where;
+    if ( $args =~ m/topic="(([^\.]+)\.)?([^"]+)"/ ) {
+        if ($2) {
 
-  #get the base name for the generated file:
-  my $basename = $drawing;
-  $basename =~ s/.svg//;
-  
-  # source file
-  my $fromFilename = TWiki::Func::getPubDir() . "/$where/$drawing";
+            # given a complete topic name, i.e. Web.TheTopic
+            $where = "$2/$3";
+        }
+        else {
 
-  # destination file
-  my $picture = "$basename-$width" . "x$height.png";
-  my $toFilename = TWiki::Func::getPubDir() . "/$where/$picture";
-  my $pictureUrl = TWiki::Func::getUrlHost()
-                   . TWiki::Func::getPubUrlPath()
-                   . "/$where/$picture";
+            # given only a topic name, use current web.
+            $where = "$web/$3";
+        }
+    }
+    else {
 
-  if (not (-e $fromFilename))
-  {
-    return SvgPluginError("can't find drawing !$drawing attched at $where.");
-  }
+        # nothing given, use current topic
+        $where = "$web/$topic";
+    }
 
-  my $svgAge = (-M $fromFilename);
-  my $pngAge = (-M $toFilename);
+    # calculate size of the generated image:
+    my ( $width, $height );
+    if ( $args =~ m/size="([0-9]+)x([0-9]+)"/ ) {
+        $width  = $1;
+        $height = $2;
+    }
+    else {
+        $defaultSize =~ m/([0-9]+)x([0-9]+)/;
+        $width  = $1;
+        $height = $2;
+    }
 
-  # (re)generate, if PNG doesn't exist yet or if PNG is older than SVG
-  if ((not defined $pngAge) or ($pngAge > $svgAge))
-  {
-    my $rsvg = new Image::LibRSVG();
-    $rsvg->convertAtMaxSize($fromFilename, $toFilename, $width, $height);
-  }
- 
-  return $pictureUrl;
+    #get the base name for the generated file:
+    my $basename = $drawing;
+    $basename =~ s/.svg//;
+
+    # source file
+    my $fromFilename = TWiki::Func::getPubDir() . "/$where/$drawing";
+
+    # destination file
+    my $picture    = "$basename-$width" . "x$height.png";
+    my $toFilename = TWiki::Func::getPubDir() . "/$where/$picture";
+    my $pictureUrl =
+        TWiki::Func::getUrlHost()
+      . TWiki::Func::getPubUrlPath()
+      . "/$where/$picture";
+
+    if ( not( -e $fromFilename ) ) {
+        return SvgPluginError(
+            "can't find drawing !$drawing attched at $where.");
+    }
+
+    my $svgAge = ( -M $fromFilename );
+    my $pngAge = ( -M $toFilename );
+
+    # (re)generate, if PNG doesn't exist yet or if PNG is older than SVG
+    if ( ( not defined $pngAge ) or ( $pngAge > $svgAge ) ) {
+        my $rsvg = new Image::LibRSVG();
+        $rsvg->convertAtMaxSize( $fromFilename, $toFilename, $width, $height );
+    }
+
+    return $pictureUrl;
 }
-
 
 # ==========================
 1;
